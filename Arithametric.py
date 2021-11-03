c83166db98cb39eddd09adeca19354686fbdae72
amardeep <amardeep.samba@gmail.com>
2021-11-03 17:28:34 +0530

    program of Arithametric

---
+++ Arithametric.py
@@ -0,0 +1,15 @@
+x = 15
+y = 10
+z1 = x + y  #Addition
+print("value of the z1 ", z1)
+
+z2 = x - y #subtraction
+print("value of the z2 " ,z2)
+
+z3 = x * y #multiply
+print("value of the z3 ", z3)
+
+z4 = x / y
+print("value of the z4 ", z4)
+z5 = x % y
+print("value of the z5 ", z5)
