8bab85a74071b19fbf03b93921851d5aed21b389
amardeep <amardeep.samba@gmail.com>
2021-11-05 11:27:57 +0530

    program of for loop

---
+++ for loop.py
@@ -0,0 +1,27 @@
+#ex : 1
+
+language = "python"
+
+for character in language:
+    print(character)
+
+#ex :2
+
+subjects = ['telugu','hindi','english','maths']
+
+for subject in subjects:
+    print("i like ",subject)
+
+#ex : 3
+
+classes = [1,2,3,4,5,6,7,8,9]
+
+for i in classes:
+    if i == 5:
+        print("class 5th is their")
+        break
+    else:
+        print("5th class is not their")
+
+
+    print(i)
